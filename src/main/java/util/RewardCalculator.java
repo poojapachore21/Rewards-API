797b9dd4de1501ce69366b535b217443b15fa337
Pooja <poojapachore21@gmail.com>
2025-09-11 13:42:08 +0530

    Replaced old project with new Spring Boot code

---
+++ src/main/java/util/RewardCalculator.java
@@ -0,0 +1,10 @@
+package util;
+
+public class RewardCalculator {
+
+    public static int calculatePoints(double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculatePoints'");
+    }
+
+}
